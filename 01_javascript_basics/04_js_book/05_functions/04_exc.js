6e70a996b2d53b4e4586af4b5ba2be1a4b87f058
Femke Knottenbelt <femknotgithub@gmail.com>
2019-07-18 05:06:14 +0000

    Finish function excerises

---
+++ 01_javascript_basics/04_js_book/05_functions/04_exc.js
@@ -0,0 +1,13 @@
+function times(number1, number2) {
+  let result = number1 * number2;
+  console.log(result);
+  return result;
+}
+
+
+let result = times(1, 1);
+result = times(result, 2);
+result = times(result, 3);
+result = times(result, 4);
+result = times(result, 5);
+
