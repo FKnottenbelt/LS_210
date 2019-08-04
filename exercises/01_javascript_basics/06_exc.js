3636449193b08ea305f639f5fb46a8364840cac9
Femke Knottenbelt <femknotgithub@gmail.com>
2019-08-04 11:43:44 +0000

    Add further exploration

--- exercises/01_javascript_basics/06_exc.js
+++ exercises/01_javascript_basics/06_exc.js
@@ -3,3 +3,7 @@ let phrase = prompt('Please enter a phrase:');
 let numOfChars = phrase.length;
 
 console.log(`There are ${numOfChars} characters in "${phrase}".`);
+
+let noSpaces = phrase.replace(/ /g, "").length;
+
+console.log(`There are ${noSpaces} characters in "${phrase}" when not counting spaces`);
