2298b50302703c49d94fee21911c4585b5fd994a
Femke Knottenbelt <femknotgithub@gmail.com>
2019-08-13 10:19:59 +0000

    Add exercise 7

---
+++ 03_logic_and_flow_control/07_exc.js
@@ -0,0 +1,20 @@
+function isPrime(number) {
+  if (number <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isPrime(1));   // false
+console.log(isPrime(2));   // true
+console.log(isPrime(3));   // true
+console.log(isPrime(43));  // true
+console.log(isPrime(55));  // false
+console.log(isPrime(0));   // false
