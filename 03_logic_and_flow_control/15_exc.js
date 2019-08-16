fa509e42dd889f004c91014968f1e03da9992369
Femke Knottenbelt <femknotgithub@gmail.com>
2019-08-16 10:33:32 +0000

    Add exercise 15

---
+++ 03_logic_and_flow_control/15_exc.js
@@ -0,0 +1,10 @@
+function trim(string) {
+  return string.trim();
+}
+
+console.log(trim('  abc  '));  // "abc"
+console.log(trim('abc   '));   // "abc"
+console.log(trim(' ab c'));    // "ab c"
+console.log(trim(' a b  c'));  // "a b  c"
+console.log(trim('      '));   // ""
+console.log(trim(''));         // ""
