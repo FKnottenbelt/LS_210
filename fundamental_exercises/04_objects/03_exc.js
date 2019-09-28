69074eefd971d7d73904713d88e6838969ab8aaf
Femke Knottenbelt <femknotgithub@gmail.com>
2019-09-28 08:19:00 +0000

    Add fundamental object exercises

---
+++ fundamental_exercises/04_objects/03_exc.js
@@ -0,0 +1,16 @@
+const array1 = ['Moe', 'Larry', 'Curly', 'Chemp', 'Harpo', 'Chico', 'Groucho', 'Zeppo'];
+const array2 = [];
+let i;
+
+for (i = 0; i < array1.length; i += 1) {
+  array2.push(array1[i]);
+}
+
+for (i = 0; i < array1.length; i += 1) {
+  if (array1[i].startsWith('C')) {
+    array1[i] = array1[i].toUpperCase();
+  }
+}
+
+console.log(array2);
+console.log(array1);
