fd389116fd843e47e5e50d0154b7de408499aae8
Femke Knottenbelt <femknotgithub@gmail.com>
2019-10-03 05:52:56 +0000

    Add fundamental object exercises

---
+++ fundamental_exercises/01_javascript_basics/10_exc.js
@@ -0,0 +1,26 @@
+const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+function integerToString(number) {
+  let result = '';
+  let remainder;
+
+  do {
+    remainder = number % 10;
+    number = Math.floor(number / 10);
+
+    result = DIGITS[remainder] + result;
+  } while (number > 0);
+
+  return result;
+}
+
+
+function signedIntegerToString(number) {
+  if (number < 0) return '-' + integerToString(-number);
+  if (number > 0) return '+' + integerToString(number);
+  return integerToString(number);
+}
+
+console.log(signedIntegerToString(4321)); // "+4321"
+console.log(signedIntegerToString(-123)); // "-123"
+console.log(signedIntegerToString(0)); // "0"
