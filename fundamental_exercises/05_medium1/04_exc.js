ae67cf679bd47be302790a06364675f55eb45502
Femke Knottenbelt <femknotgithub@gmail.com>
2019-10-07 09:58:38 +0000

    Add fundamental medium1 exercises

---
+++ fundamental_exercises/05_medium1/04_exc.js
@@ -0,0 +1,32 @@
+function getSelectedColumns(numbers, cols) {
+  const result = [];
+
+  for (let i = 0, outerLength = numbers; i < outerLength; i += 1) {
+    for (let j = 0, innerLength = cols; j < innerLength; j += 1) {
+      if (!result[j]) {
+        result[j] = [];
+      }
+
+      result[j][i] = numbers[i][cols[j]];
+    }
+  }
+
+  return result;
+}
+
+// given the following arrays of number arrays
+const array1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const array2 = [[1, 2, 3], [1, 2, 3], [1, 2, 3]];
+
+// `array1` in row/column format
+// [[1, 2, 3],
+//  [4, 5, 6],
+//  [7, 8, 9]]
+
+console.log(getSelectedColumns(array1, [0])); // [[1]];            expected: [[1, 4, 7]]
+console.log(getSelectedColumns(array1, [0, 2])); // [[1, 4], [3, 6]]; expected: [[1, 4, 7], [3, 6, 9]]
+console.log(getSelectedColumns(array2, [1, 2])); // [[2, 2], [3, 3]]; expected: [[2, 2, 2], [3, 3, 3]]
+
+// just using let in stead of var solves the problem. But renaming both
+// lenght variables is of course also better (and would by itself also
+// solve it)
